1adcc761010b84e0f04b2c8e378678f1ed891cf3
jy-hwang <stratojy@gmail.com>
2025-05-16 11:49:38 +0900

    feat. element-plus 라이브러리 추가
    1. element-plus 라이브러리 추가
    2. css, js, locale 등을 사용할 수 있게 내용 작성

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+import "element-plus/dist/index.css";
+export default {};
+</script>
 
 <style>
 #app {
