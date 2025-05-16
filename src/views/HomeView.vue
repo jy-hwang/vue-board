0653db17daf80e07d95b6c0c98eeddb37b29b9b5
jy-hwang <stratojy@gmail.com>
2025-05-16 11:38:41 +0900

    feat. axios를 이용하여 서버에서 데이터 가져오기
    1. 가짜 게시판 데이터가 있는 jsonplaceholder.typicode.com 사이트의 api 를 이용하도록 함.
    2. 단건 게시글 메서드를 만들어서 페이지가 처음 시작될때 한번 실행해서 console.log 로 출력함.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>{{ title }}</h1>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import apiBoard from "@/api/board";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      title: "hello",
+    };
+  },
+  mounted() {
+    apiBoard
+      .getArticle(1)
+      .then(function (response) {
+        console.log("getArticle : ", response);
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+  },
+};
 </script>
